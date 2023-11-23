1d09877ad6d45ea3e98fa6fe3e02e9896e908666
Carmin001 <138111473+Carmin001@users.noreply.github.com>
2023-11-23 17:19:23 +0100

    Creation of ClockTrigger class

---
+++ src/main/java/com/example/rminder/model/ClockTrigger.java
@@ -0,0 +1,34 @@
+package com.example.rminder.model;
+import java.time.LocalTime;
+
+public class ClockTrigger implements Trigger {
+    private LocalTime time;
+
+    public ClockTrigger(LocalTime time) {
+        this.time = time;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    @Override
+    public boolean verifyTrigger() {
+        LocalTime currentTime = LocalTime.now();
+        if (currentTime.isAfter(time)) {
+            System.out.println("ClockTrigger is active\n");
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "ClockTrigger" + time + ";";
+    }
+
+}
